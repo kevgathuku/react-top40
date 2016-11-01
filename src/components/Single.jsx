814ecb270b91928a7b5ec54d287daeda552ea186
Kevin Ndung'u <kevgathuku@gmail.com>
2016-11-01 10:50:51 +0300

    Move singles functionality to Single.jsx
    Refactor the components such that the Top40 component deals with all
    the general functionality and the Single component deals with the
    single track functionality

--- src/components/Single.jsx
+++ src/components/Single.jsx
@@ -1,34 +1,28 @@
 import React from 'react';
-import Masonry from 'react-masonry-component';
-
-const masonryOptions = {
-  transitionDuration: 0,
-  gutter: 20
-};
 
 class Single extends React.Component {
-  render() {
-    let renderSingle = function(single) {
-      let options = [200, 250, 300, 350, 400];
-      let width = options[Math.floor(Math.random() * options.length)];
-      return (
-        <div className="item" key={single.position}>
-          <h5>{single.title} <small>{single.artist}</small></h5>
-          <img alt={single.title} src={`http://lorempixel.com/${width}/400`}/>
-        </div>
-      );
-    };
+  static propTypes = {
+    track: React.PropTypes.object
+  }
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      track: this.props.track
+    };
+  }
+  render() {
     return (
-      <Masonry className={'grid'} options={masonryOptions}>
-        {this.props.entries.map(renderSingle)}
-      </Masonry>
+      <div className="item">
+        <h5>{this.state.track.title} <small>{this.state.track.artist}</small></h5>
+          {this.state.track.position === 25
+            ? <img alt={this.state.track.title} src="http://is3.mzstatic.com/image/thumb/Music62/v4/f7/93/79/f79379c8-86c0-fca6-6d46-5d027d6936e7/source/100x100bb.jpg"/>
+            : <br/>}
+      </div>
     );
   }
 }
 
-Single.propTypes = {
-  entries: React.PropTypes.arrayOf(React.PropTypes.object)
-};
-
 export default Single;
+
+// <img alt={single.title} src={`http://lorempixel.com/${width}/400`}/>
--- src/components/Top40.jsx
+++ src/components/Top40.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
-
+import Masonry from 'react-masonry-component';
 import Top40Actions from '../actions/Top40Actions';
 import Top40Store from '../stores/Top40Store';
 import Single from './Single.jsx';
+
+const masonryOptions = {
+  transitionDuration: 0,
+  gutter: 20
+};
 
 class Top40 extends React.Component {
   constructor() {
@@ -19,15 +24,22 @@ class Top40 extends React.Component {
 
   populateSingles = () => {
     let data = Top40Store.getData();
-    this.setState({singles: data});
+    this.setState({singles: data.entries});
   }
 
   render() {
+    let renderSingle = function(single) {
+      return (
+        <Single track={single} key={single.position}/>
+      );
+    };
     return (
       <div>
         <h1>The Official UK Top 40 Singles Chart</h1>
-        {this.state.singles
-          ? <Single entries={this.state.singles.entries}/>
+        {this.state.singles && this.state.singles.length > 1
+          ? <Masonry className={'grid'} options={masonryOptions}>
+             {this.state.singles.map(renderSingle)}
+            </Masonry>
           : <p>Loading...</p>}
       </div>
     );
